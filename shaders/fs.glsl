e23a09e715cd086e1bafed8f9e619162fa1a4307
Qu Xing Zhu <125479746+GitGud4040@users.noreply.github.com>
2023-06-28 23:57:01 +0200

    small update

--- shaders/fs.glsl
+++ shaders/fs.glsl
@@ -18,7 +18,6 @@ out vec4 outputColor;
 // fragment shader
 void main()
 {
-    //outputColor = texture(diffuseTexture, uv) * lightColor + 0.5 * normalWorld;
     vec3 L = lightPosition - positionWorld.xyz; 
     float attenuation = 1.0 / dot(L, L);
     float NdotL = max(0, dot(normalize(normalWorld.xyz), normalize(L))); 
@@ -26,11 +25,12 @@ void main()
 
     //specular light
     vec3 R = lightPosition - 2*dot(lightPosition, normalWorld.xyz) * normalWorld.xyz;
-    //float VdotR = max(pow(dot(normalize(viewD), normalize(R)),20), 0);
-    float VdotR = pow(max(dot(normalize(viewD), normalize(R)),0),20);
+    float VdotR = pow(max(dot(normalize(viewD), normalize(R)),0),25);
     float specualarStrenght = 0.005f;
     //vec3 specularColor = lightColor.rgb * specularLight.rgb * specualarStrenght;
     //outputColor = vec4(lightColor.rgb * diffuseColor * attenuation * NdotL, 1.0) + (VdotR * specualarStrenght * specularColor, 1 );
+
+    //phong illumination
     outputColor = vec4(lightColor.rgb * diffuseColor * attenuation * NdotL, 1.0) + (VdotR * lightColor * specualarStrenght);
 
     
