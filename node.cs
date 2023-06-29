68ea247a016aadb35b324176ee228659583f5b71
Luka <luukberkers@protonmail.com>
2023-06-29 13:06:15 +0200

    Add some debugging output for rendering

--- node.cs
+++ node.cs
@@ -4,17 +4,18 @@ namespace Rasterization;
 
 public class Node
 {
+    private readonly string _debugName;
     private readonly ISceneObject? _obj;
     private readonly Shader _shader;
 
-    public Node(ISceneObject? obj, Shader shader, List<Node>? children = null)
+    public Node(ISceneObject? obj, Shader shader, List<Node>? children = null, string? name = null)
     {
         _obj = obj;
         _shader = shader;
         Children = children ?? new List<Node>();
+        _debugName = name ?? _obj?.Mesh.Filename ?? "Unknown object";
     }
 
-    // Node world;
     public List<Node> Children { get; }
 
     public void Update(float frameDuration)
@@ -28,6 +29,8 @@ public class Node
         var objectToParent = _obj?.ObjectTransformation ?? Matrix4.Identity;
         var objectToWorld = objectToParent * parentToWorld;
         var objectToScreen = objectToWorld * worldToScreen;
+        
+        Console.Error.WriteLine($"Render: {_debugName}");
         _obj?.Mesh.Render(_shader, objectToScreen, objectToWorld, _obj.Texture);
 
         foreach (var child in Children) child.Render(worldToScreen, objectToWorld);
